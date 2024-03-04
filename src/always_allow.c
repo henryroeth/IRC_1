99a748e096e5074e0bf6e463c7a680514219aead
Henry <roethhk@hiram.edu>
2024-03-04 23:43:39 +0000

    udpates

---
+++ src/always_allow.c
@@ -0,0 +1,27 @@
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    // Always return success
+    return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    // Always return success
+    return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    // Always return success
+    return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    // Always return success
+    return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    // Always return success
+    return PAM_SUCCESS;
+}
